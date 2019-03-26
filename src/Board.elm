0c0595cfb31c9393932bd7369066c5c2a5ecd2fa
KasMA1990 <kasma1990_proxy@outlook.com>
2019-03-26 22:06:48 +0100

    Final stab

--- src/Board.elm
+++ src/Board.elm
@@ -1,4 +1,7 @@
-module Board exposing (Board, BoardIndex, File(..), Move, MoveError(..), Piece(..), Player(..), Position, PositionError(..), Rank(..), SquareState(..), init)
+module Board exposing (Board, File(..), Move, MoveError(..), Piece(..), Player(..), Position, Rank(..), SquareState(..), blackInit, executeMove, get, getValidMoves, init, whiteInit)
+
+-- I have attempted to build the Board module to be a general chess board that different clients could use with a sensible interface.
+-- In this case the Chess module is the client
 
 import Array exposing (Array, set)
 
@@ -7,11 +10,22 @@ type Board
     = Board Squares
 
 
+
+-- I have chosen to represent my board in an array, since I will be doing a lot of reads;
+-- for example, to determine valid moves. It is also nice if I want to let the (unimplemented) view function work with slices
+-- for rows for example
+
+
 type alias Squares =
     Array SquareState
 
 
-init : List ( Piece, Position ) -> Board
+
+-- Any list of pieces and positions can be loaded, allowing the caller to decide to load a game with fresh positions,
+-- or a list of pieces from a prior unfinished game maybe. Also makes it easier to test
+
+
+init : List ( Player, Piece, Position ) -> Board
 init initPositions =
     let
         emptySquares =
@@ -20,14 +34,17 @@ init initPositions =
     Board (occupySquares initPositions emptySquares)
 
 
-occupySquares : List ( Piece, Position ) -> Squares -> Squares
+occupySquares : List ( Player, Piece, Position ) -> Squares -> Squares
 occupySquares pieces squares =
     case pieces of
-        ( piece, pos ) :: [] ->
-            set (unpack (toIndex pos)) (Taken piece) squares
+        [] ->
+            squares
 
-        ( piece, pos ) :: tail ->
-            set (unpack (toIndex pos)) (Taken piece) (occupySquares tail)
+        ( player, piece, pos ) :: [] ->
+            set (unpack (toIndex pos)) (Taken ( player, piece )) squares
+
+        ( player, piece, pos ) :: tail ->
+            set (unpack (toIndex pos)) (Taken ( player, piece )) (occupySquares tail squares)
 
 
 unpack : BoardIndex -> Int
@@ -35,18 +52,30 @@ unpack (BoardIndex index) =
     index
 
 
+get : Position -> Board -> SquareState
+get pos board =
+    getByIndex (toIndex pos) board
+
+
+getByIndex : BoardIndex -> Board -> SquareState
+getByIndex (BoardIndex index) (Board squares) =
+    -- Free is used as a default value, though it should never be used in practice;
+    -- Position is constrained, so as to only match a valid index in the array
+    Maybe.withDefault Free (Array.get index squares)
+
+
 type SquareState
     = Free
-    | Taken Piece
+    | Taken ( Player, Piece )
 
 
 type Piece
-    = King Player
-    | Queen Player
-    | Rook Player
-    | Bishop Player
-    | Knight Player
-    | Pawn Player
+    = King
+    | Queen
+    | Rook
+    | Bishop
+    | Knight
+    | Pawn
 
 
 type Player
@@ -92,18 +121,83 @@ type PositionError
     = OffBoard
 
 
-getValidMoves : Player -> Board -> Piece -> List Move
+getValidMoves : Player -> Board -> List ( Piece, List Move )
+getValidMoves player board =
+    let
+        (Board squares) =
+            board
+
+        playerPositions =
+            Array.toIndexedList squares
+                |> List.filterMap (toPlayerSquare player)
+    in
+    List.map (\( index, piece ) -> ( piece, getValidMovesForPiece board index piece )) playerPositions
+
+
+getValidMovesForPiece : Board -> BoardIndex -> Piece -> List Move
+getValidMovesForPiece board index piece =
+    -- TODO Determine actual valid moves for each piece
+    List.filterMap Result.toMaybe [ addRank board 1 index ]
+
+
+toPlayerSquare : Player -> ( Int, SquareState ) -> Maybe ( BoardIndex, Piece )
+toPlayerSquare player ( index, square ) =
+    case square of
+        Free ->
+            Nothing
+
+        Taken ( occupantPlayer, piece ) ->
+            if occupantPlayer == player then
+                Just ( BoardIndex index, piece )
+
+            else
+                Nothing
+
+
+
+-- I only want to allow executing a Move on the board it was constructed from, so the move contains a reference to its board.
+-- In Rust, I could have made it a compile time error to execute a stale move, instead of a run time error,
+-- since Rust allows me to consume data of certain types completely, forcing the caller to pass in the necessary data
+-- without letting them keep references to it
 
 
 executeMove : Board -> Move -> Result MoveError Board
-executeMove board (Move move) =
-    if move.board == board then
+executeMove board move =
+    let
+        (Move actualMove) =
+            move
+    in
+    if actualMove.board == board then
         case board of
             Board squares ->
-                Ok (Debug.todo "Update both from position and to position in the board")
+                let
+                    ( player, piece ) =
+                        case getByIndex actualMove.from board of
+                            Taken x ->
+                                x
+
+                            -- TODO Move types can only be constructed from squares that are Taken, and I already verified
+                            -- that the Move is based on the current board, so this case should be impossible. How to best handle it?
+                            Free ->
+                                Debug.todo "How to handle this best?"
+
+                    boardWithLiftedPiece =
+                        updateFromPosition squares move
+                in
+                Ok (Board (updateToPosition player piece boardWithLiftedPiece move))
 
     else
         Err StaleMove
+
+
+updateToPosition : Player -> Piece -> Squares -> Move -> Squares
+updateToPosition player piece squares (Move move) =
+    set (unpack move.to) (Taken ( player, piece )) squares
+
+
+updateFromPosition : Squares -> Move -> Squares
+updateFromPosition squares (Move move) =
+    set (unpack move.from) Free squares
 
 
 addRank : Board -> Int -> BoardIndex -> Result PositionError Move
@@ -156,8 +250,16 @@ addFile board files (BoardIndex index) =
         Err OffBoard
 
 
+
+-- Human readable type to be used for the public interface when users want to deal with positions and not just moves
+
+
 type alias Position =
     ( File, Rank )
+
+
+
+-- Internal new-type around Int, to make sure the indices don't get mixed with other numbers in the program
 
 
 type BoardIndex
@@ -272,6 +374,9 @@ toRank index =
         7 ->
             Eight
 
+        _ ->
+            Eight
+
 
 toFile : Int -> File
 toFile index =
@@ -303,3 +408,48 @@ toFile index =
 
         7 ->
             H
+
+        _ ->
+            H
+
+
+whiteInit : List ( Player, Piece, Position )
+whiteInit =
+    [ ( White, Rook, ( A, One ) )
+    , ( White, Knight, ( B, One ) )
+    , ( White, Bishop, ( C, One ) )
+    , ( White, Queen, ( D, One ) )
+    , ( White, King, ( E, One ) )
+    , ( White, Bishop, ( F, One ) )
+    , ( White, Knight, ( G, One ) )
+    , ( White, Rook, ( H, One ) )
+    , ( White, Pawn, ( A, Two ) )
+    , ( White, Pawn, ( B, Two ) )
+    , ( White, Pawn, ( C, Two ) )
+    , ( White, Pawn, ( D, Two ) )
+    , ( White, Pawn, ( E, Two ) )
+    , ( White, Pawn, ( F, Two ) )
+    , ( White, Pawn, ( G, Two ) )
+    , ( White, Pawn, ( H, Two ) )
+    ]
+
+
+blackInit : List ( Player, Piece, Position )
+blackInit =
+    [ ( Black, Rook, ( A, Eight ) )
+    , ( Black, Knight, ( B, Eight ) )
+    , ( Black, Bishop, ( C, Eight ) )
+    , ( Black, Queen, ( D, Eight ) )
+    , ( Black, King, ( E, Eight ) )
+    , ( Black, Bishop, ( F, Eight ) )
+    , ( Black, Knight, ( G, Eight ) )
+    , ( Black, Rook, ( H, Eight ) )
+    , ( Black, Pawn, ( A, Seven ) )
+    , ( Black, Pawn, ( B, Seven ) )
+    , ( Black, Pawn, ( C, Seven ) )
+    , ( Black, Pawn, ( D, Seven ) )
+    , ( Black, Pawn, ( E, Seven ) )
+    , ( Black, Pawn, ( F, Seven ) )
+    , ( Black, Pawn, ( G, Seven ) )
+    , ( Black, Pawn, ( H, Seven ) )
+    ]
--- src/Chess.elm
+++ src/Chess.elm
@@ -1,6 +1,6 @@
 module Chess exposing (GameState, init)
 
-import Board exposing (Board, File(..), Piece(..), Player(..), Position, Rank(..))
+import Board exposing (Board, File(..), Move, MoveError(..), Piece(..), Player(..), Position, Rank(..), blackInit, whiteInit)
 
 
 type GameState
@@ -10,54 +10,48 @@ type GameState
 type alias State =
     { board : Board
     , currentTurn : Player
+    , potentialMoves : List ( Piece, List Move )
     }
 
 
 init : GameState
 init =
+    let
+        currentPlayer =
+            White
+
+        board =
+            Board.init (append whiteInit blackInit)
+    in
     GameState
-        { board = Board.init (append whiteInit blackInit)
-        , currentTurn = White
+        { board = board
+        , currentTurn = currentPlayer
+        , potentialMoves = Board.getValidMoves currentPlayer board
         }
 
 
-whiteInit : List ( Piece, Position )
-whiteInit =
-    [ ( Rook White, ( A, One ) )
-    , ( Knight White, ( B, One ) )
-    , ( Bishop White, ( C, One ) )
-    , ( Queen White, ( D, One ) )
-    , ( King White, ( E, One ) )
-    , ( Bishop White, ( F, One ) )
-    , ( Knight White, ( G, One ) )
-    , ( Rook White, ( H, One ) )
-    , ( Pawn White, ( A, Two ) )
-    , ( Pawn White, ( B, Two ) )
-    , ( Pawn White, ( C, Two ) )
-    , ( Pawn White, ( D, Two ) )
-    , ( Pawn White, ( E, Two ) )
-    , ( Pawn White, ( F, Two ) )
-    , ( Pawn White, ( G, Two ) )
-    , ( Pawn White, ( H, Two ) )
-    ]
+makeMove : GameState -> Move -> GameState
+makeMove fullState move =
+    let
+        (GameState state) =
+            fullState
 
+        nextPlayer =
+            case state.currentTurn of
+                White ->
+                    Black
 
-blackInit : List ( Piece, Position )
-blackInit =
-    [ ( Rook Black, ( A, Eight ) )
-    , ( Knight Black, ( B, Eight ) )
-    , ( Bishop Black, ( C, Eight ) )
-    , ( Queen Black, ( D, Eight ) )
-    , ( King Black, ( E, Eight ) )
-    , ( Bishop Black, ( F, Eight ) )
-    , ( Knight Black, ( G, Eight ) )
-    , ( Rook Black, ( H, Eight ) )
-    , ( Pawn Black, ( A, Seven ) )
-    , ( Pawn Black, ( B, Seven ) )
-    , ( Pawn Black, ( C, Seven ) )
-    , ( Pawn Black, ( D, Seven ) )
-    , ( Pawn Black, ( E, Seven ) )
-    , ( Pawn Black, ( F, Seven ) )
-    , ( Pawn Black, ( G, Seven ) )
-    , ( Pawn Black, ( H, Seven ) )
-    ]
+                Black ->
+                    White
+    in
+    case Board.executeMove state.board move of
+        Ok newBoard ->
+            GameState
+                { board = newBoard
+                , currentTurn = nextPlayer
+                , potentialMoves = Board.getValidMoves nextPlayer newBoard
+                }
+
+        -- TODO What would nice error handling look like in this case?
+        Err StaleMove ->
+            fullState
